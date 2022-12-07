1f554b4cf302112bba5b3df216323a09607d8681
Azkarul <118016842+Azkarul@users.noreply.github.com>
2022-12-07 11:04:54 +0600

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+
+
+$(function(){
+
+    // counter
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+      });
+      $('.counter').addClass('animated fadeInDownBig');
+      $('h3').addClass('animated fadeIn');
+    // counter
+});
